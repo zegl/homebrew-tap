afe9231708054ae24cec6d567af8482f67ac2400
Gustav Westling <gustav@westling.dev>
2022-11-22 15:52:07 +0100

    git-linearize: update to version 13, add shit wrapper

--- Formula/git-shit.rb
+++ Formula/git-shit.rb
@@ -1,14 +1,12 @@
-class GitLinearize < Formula
-  desc "Git Linearize"
+class GitShit < Formula
+  desc "Git Linearize - Short Git Wrapper"
   homepage "https://github.com/zegl/extremely-linear"
   version "13.0.0"
 
   url "https://github.com/zegl/extremely-linear/archive/0000013089c3be9556c6004e91dbd19a669f68d2.tar.gz"
   sha256 "7759530bbd9c13fd8b3f0f90ae0558cb9c59c177805e1c10e4adec00530a8ce8"
 
-  depends_on "lucky-commit"
-
   def install
-    bin.install "git-linearize"
+    bin.install "shit"
   end
 end
