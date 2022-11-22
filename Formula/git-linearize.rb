97d1d5be6420f4b01e55331dd78f302190253267
Gustav Westling <gustav@westling.dev>
2022-11-22 10:43:37 +0100

    git-linearize: declare dependency on githashcrash

--- Formula/git-linearize.rb
+++ Formula/git-linearize.rb
@@ -6,6 +6,8 @@ class GitLinearize < Formula
   url "https://github.com/zegl/extremely-linear/archive/000600d40bb7b6fab9eaee58efc2fe29f9b00b0f.tar.gz"
   sha256 "d99523339b8405ea2ee9132357329afb5a57320a51c3d9159418acd20f31378c"
 
+  depends_on "zegl/tap/githashcrash"
+
   def install
     bin.install "git-linearize"
   end
