4efd2f54645ba2b980a241e3f8e3e1986e7c7c47
Gustav Westling <gustav@westling.dev>
2022-11-22 11:08:30 +0100

    git-linearize: 1.0.0008

--- Formula/git-linearize.rb
+++ Formula/git-linearize.rb
@@ -1,10 +1,10 @@
 class GitLinearize < Formula
   desc "Git Linearize"
   homepage "https://github.com/zegl/extremely-linear"
-  version "1.0.0006"
+  version "1.0.0008"
 
-  url "https://github.com/zegl/extremely-linear/archive/000600d40bb7b6fab9eaee58efc2fe29f9b00b0f.tar.gz"
-  sha256 "d99523339b8405ea2ee9132357329afb5a57320a51c3d9159418acd20f31378c"
+  url "https://github.com/zegl/extremely-linear/archive/000800164e6524194f0ec377dbabdd10be7e489a.tar.gz"
+  sha256 "25222657464c482845f0d78abae3d793346da5b5f560f0563ea54fc476e94f08"
 
   depends_on "zegl/tap/githashcrash"
 
