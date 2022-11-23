1b4a00cb40541955e282722e49e4d7787a924a39
Gustav Westling <gustav@westling.dev>
2022-11-23 16:45:31 +0100

    git-shit,git-linearize: 30.0.1

--- Formula/git-shit.rb
+++ Formula/git-shit.rb
@@ -1,10 +1,10 @@
 class GitShit < Formula
   desc "Git Linearize - Short Git Wrapper"
   homepage "https://github.com/zegl/extremely-linear"
-  version "30.0.0"
+  version "30.0.1"
 
-  url "https://github.com/zegl/extremely-linear/archive/00000230ec6681ecce7f155f7d9ff4b21fabdab2.tar.gz"
-  sha256 "e4da11539cd8eec2fb49ec8a34e8587c0d02c7f85b05a6604f4b470d454475cd"
+  url "https://github.com/zegl/extremely-linear/archive/00000300cc6862fc32cbcd1e774bf54dd64e4e70.tar.gz"
+  sha256 "a0f3220adc8f2577b1ecb7ea9edd8953612c7d627596ba76c09312e876737b0c"
 
   def install
     bin.install "shit"
