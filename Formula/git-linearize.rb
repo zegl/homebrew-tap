1fe02306dbb9a9cf0352c3dd00c47f9d4ca058bb
Gustav Westling <gustav@westling.dev>
2022-12-07 09:49:02 +0100

    Update git-linearize to 53

--- Formula/git-linearize.rb
+++ Formula/git-linearize.rb
@@ -1,10 +1,10 @@
 class GitLinearize < Formula
   desc "Git Linearize"
   homepage "https://github.com/zegl/extremely-linear"
-  version "51.0.0"
+  version "53.0.0"
 
-  url "https://github.com/zegl/extremely-linear/archive/000005106b8815b78a69b90a63cee63600943f34.tar.gz"
-  sha256 "44a66bc91fb3d931e6247487b67342fda0b43a4fdcb625e9fd2cfa2ad5fc26dd"
+  url "https://github.com/zegl/extremely-linear/archive/00000530adb0b1ce0028bb0496a1fcd5357e78a1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   depends_on "lucky-commit"
 
