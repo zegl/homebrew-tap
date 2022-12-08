ae6cee2b869601818919b2b884d279cf309dfbba
Gustav Westling <gustav@westling.dev>
2022-12-08 18:05:27 +0100

    Update git-linearize to 53

--- Formula/git-shit.rb
+++ Formula/git-shit.rb
@@ -4,7 +4,7 @@ class GitShit < Formula
   version "53.0.0"
 
   url "https://github.com/zegl/extremely-linear/archive/00000530adb0b1ce0028bb0496a1fcd5357e78a1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "704b118ca983a3328b43373e5dc2dca3a3bf2b5ea48b4675d18653ff652affe8"
 
   def install
     bin.install "shit"
